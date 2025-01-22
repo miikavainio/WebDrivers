5baefaad225857f7287c5fdde5f739dd338a1ebe
Miika <vainiomiika@hotmail.com>
2025-01-22 19:42:47 +0200

    Driver initiation

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 import pytesseract
+
+driver = webdriver.Chrome()
+
+
+try:
+    driver.get("https://www.google.com")
+    driver.maximize_window()
+    
+finally:
+    driver.quit()
