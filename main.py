3ae8fa6c61bcbfe724e7d647ea0c33b9efaa8119
Miika <vainiomiika@hotmail.com>
2025-01-23 16:27:11 +0200

    Used options instead to maximize.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 import pytesseract
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(options=chrome_options)
 
 #Tesseract path
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -13,7 +19,7 @@ driver = webdriver.Chrome()
 
 try:
     driver.get("https://www.google.com")
-    driver.maximize_window()
+    
     
 finally:
     driver.quit()
